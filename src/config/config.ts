8d7ff4186240a6ea08e23418c53f635bb815a000
ucuo <fajarmufid01@gmail.com>
2025-06-14 20:17:43 +0700

    aa

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,17 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
 class Config {
-    port: Number = Number(process.env.PORT) || 3000;
+    port: number = Number(process.env.PORT) || 3000;
+
+    dbHost: string = process.env.DB_HOST || "127.0.0.1";
+    dbPort: number = Number(process.env.DB_PORT) || 3306;
     dbUsername: string = process.env.DB_USERNAME || "root";
     dbPassword: string = process.env.DB_PASSWORD || "";
-    dbName: string = process.env.DB_DATABASE;
+    dbName: string = process.env.DB_DATABASE || "";
+
     jwtKey: string = "abc";
     jwtExpiration: string = "24h";
 }
 
-export default new Config;
+export default new Config();
