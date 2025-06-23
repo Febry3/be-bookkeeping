236c35517c8b21b67e719be136df39201e587d84
ucuo <fajarmufid01@gmail.com>
2025-06-23 13:56:26 +0700

    ubah lagi

--- src/api/financial/financial.router.ts
+++ src/api/financial/financial.router.ts
@@ -14,23 +14,5 @@ router.get('/assets/:id', authenticateUser.authenticateUser as RequestHandler, f
 router.patch('/assets/:id', authenticateUser.authenticateUser as RequestHandler, financialController.updateAsset as RequestHandler);
 router.delete('/assets/:id', authenticateUser.authenticateUser as RequestHandler, financialController.deleteAsset as RequestHandler);
 
-// =============================================
-// =========== RUTE UNTUK EKUITAS (EQUITY) =======
-// =============================================
-router.post('/equities', authenticateUser.authenticateUser as RequestHandler, financialController.createEquity as RequestHandler);
-router.get('/equities', authenticateUser.authenticateUser as RequestHandler, financialController.getAllEquities as RequestHandler);
-router.get('/equities/:id', authenticateUser.authenticateUser as RequestHandler, financialController.getEquityById as RequestHandler);
-router.patch('/equities/:id', authenticateUser.authenticateUser as RequestHandler, financialController.updateEquity as RequestHandler);
-router.delete('/equities/:id', authenticateUser.authenticateUser as RequestHandler, financialController.deleteEquity as RequestHandler);
 
-// =============================================
-// ======== RUTE UNTUK LIABILITAS (LIABILITY) ====
-// =============================================
-router.post('/liabilities', authenticateUser.authenticateUser as RequestHandler, financialController.createLiability as RequestHandler);
-router.get('/liabilities', authenticateUser.authenticateUser as RequestHandler, financialController.getAllLiabilities as RequestHandler);
-router.get('/liabilities/:id', authenticateUser.authenticateUser as RequestHandler, financialController.getLiabilityById as RequestHandler);
-router.patch('/liabilities/:id', authenticateUser.authenticateUser as RequestHandler, financialController.updateLiability as RequestHandler);
-router.delete('/liabilities/:id', authenticateUser.authenticateUser as RequestHandler, financialController.deleteLiability as RequestHandler);
-
-// Ekspor router agar bisa digunakan di file utama (app.ts atau index.ts)
 export { router };
